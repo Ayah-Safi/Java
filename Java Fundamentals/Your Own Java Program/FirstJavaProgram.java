a5c3acb6b922791440c84ddc6e7fcb41a4ec862b
Ayah Safi <ayaadnansafi@gmail.com>
2023-12-26 11:21:01 +0200

    upload first assignment

---
+++ Java Fundamentals/Your Own Java Program/FirstJavaProgram.java
@@ -0,0 +1,10 @@
+class FirstJavaProgram {
+    public static void main(String[] args){
+        System.out.println("My name is Ayah Safi");
+        System.out.println("I am 100 years old");        
+        System.out.println("My hometown is Ramallah, Palestine");
+
+
+    }
+
+}
