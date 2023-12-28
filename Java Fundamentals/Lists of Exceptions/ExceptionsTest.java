56f80fbb26dd762425921aae59758f34c180f05d
Ayah Safi <ayaadnansafi@gmail.com>
2023-12-28 23:10:59 +0200

    Lists of Exceptions Assignment Upload

---
+++ Java Fundamentals/Lists of Exceptions/ExceptionsTest.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class ExceptionsTest {
+    public static void main(String[] args) {
+
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+    
+        for(int i = 0; i < myList.size(); i++) {
+            try{
+                Integer castedValue = (Integer) myList.get(i);
+                System.out.println("---Print Out Successfully Done---");
+                System.out.println(castedValue);
+            } catch (ClassCastException e){
+                System.out.println("---This is Not an Integer---");
+            }       
+        }
+    }
+}
