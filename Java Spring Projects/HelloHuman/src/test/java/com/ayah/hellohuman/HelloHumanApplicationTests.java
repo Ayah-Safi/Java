49cb7d14ecca7576d1abf7a9610a7b519234ffbf
Ayah Safi <ayaadnansafi@gmail.com>
2024-01-02 14:54:47 +0200

    Hello Human Assignment Upload

---
+++ Java Spring Projects/HelloHuman/src/test/java/com/ayah/hellohuman/HelloHumanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayah.hellohuman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloHumanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ Java Spring Projects/helloWorld/src/main/java/com/codingdojo/helloworld/HomeController.java
@@ -0,0 +1,14 @@
+package com.codingdojo.helloworld;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HomeController {
+	@RequestMapping("/")
+	public String index() {
+		return "Hello World";
+	}
+
+}
